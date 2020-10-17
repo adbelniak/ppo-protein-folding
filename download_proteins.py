99e9c53260c32b97555edf7223fb4fa37b34ffe3
adam <adam.belniak@balticdatascience.com>
2020-10-17 22:35:28 +0200

    fix issue with downloading pdb

--- download_proteins.py
+++ download_proteins.py
@@ -2,7 +2,9 @@ import requests
 import os
 
 with open('protein_data/protein_list.txt') as f:
-    for pdb in f:
+    for pdb in f.readlines():
+        pdb = pdb.strip()
         r = requests.get(pdb, allow_redirects=True)
         file_name = pdb.split('/')[-1]
+        print("downloading pdb file:" + file_name)
         open(os.path.join('protein_data', 'short', file_name.lower()), 'wb').write(r.content)
