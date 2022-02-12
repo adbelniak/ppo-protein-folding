dcf5cd95efc18766ed888af5ab2c2f4bed71ee79
Adam Belniak <a.belniak@fudosecurity.com>
2022-02-12 15:50:18 +0100

    enable best distance reward

--- gym-rosetta/gym_rosetta/envs/protein_fold_env.py
+++ gym-rosetta/gym_rosetta/envs/protein_fold_env.py
@@ -260,7 +260,7 @@ class ProteinFoldEnv(gym.Env, utils.EzPickle):
         best_distance_rew = 0
 
         if self.best_distance > distance:
-            # best_distance_rew = self.get_distance_reward(distance)
+            best_distance_rew = self.get_best_distance_reward(distance)
             self.best_distance = distance
         if energy < self.best_energy:
             self.best_energy = energy
